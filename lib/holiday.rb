f794d0b34ec165c0b9b748ae61d13fc030eb31bc
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:49:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 
   holiday_hash.each do |season, holiday|
     if holiday == :memorial_day
-      :memorial_day << supply
+      memorial_day << supply
     end
   end
 

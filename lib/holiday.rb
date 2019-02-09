c13fd06a92dd8c135bf666723f760a8d9fbbcd4d
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:56:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter]
+  holiday_hash[:winter].values.flatten
   
 end
 

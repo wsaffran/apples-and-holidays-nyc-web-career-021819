e40cf32dc489956c2c9cf0519f9d0d25c4e7d90e
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:59:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
   end
-end
     
 
 end
